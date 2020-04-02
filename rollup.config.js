0dc490c5ea5bf04e27c8b10871f6746fcc4438c1
Artium <artium.wasse@gmail.com>
2020-04-02 02:52:38 +0200

    Projet init

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import { terser } from "rollup-plugin-terser";
+// doc : https://github.com/thgh/rollup-plugin-serve
+import serve from "rollup-plugin-serve";
+// doc : https://github.com/rollup/rollup-plugin-babel
+import babel from "rollup-plugin-babel";
+// doc : https://github.com/thgh/rollup-plugin-livereload
+import livereload from "rollup-plugin-livereload";
+
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      entryFileNames: "bundle.js",
+      dir: "dist",
+      format: "iife"
+    },
+    {
+      entryFileNames: "bundle.min.js",
+      dir: "dist",
+      format: "iife",
+      name: "version",
+      plugins: [terser()]
+    }
+  ],
+  watch: { include: ["src/**", "public/**"] },
+  plugins: [
+    serve({
+      contentBase: ["dist", "public"],
+      open: true
+    }),
+    livereload({
+      watch: ["dist", "public"]
+    }),
+    babel({
+      include: "src/**"
+    })
+  ]
+};
