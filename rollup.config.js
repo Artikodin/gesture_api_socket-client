b48612d0b7c9d742d6c74a357f1d7274cddab8ba
Artium <artium.wasse@gmail.com>
2020-04-05 19:05:16 +0200

    refactor(rollup): Change serve plugin to dev plugin to run dev server

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
 import { terser } from 'rollup-plugin-terser';
-// doc : https://github.com/thgh/rollup-plugin-serve
-import serve from 'rollup-plugin-serve';
 // doc : https://github.com/rollup/rollup-plugin-babel
 import babel from 'rollup-plugin-babel';
 // doc : https://github.com/thgh/rollup-plugin-livereload
 import livereload from 'rollup-plugin-livereload';
 // doc : https://github.com/rollup/plugins/tree/master/packages/node-resolve
 import resolve from '@rollup/plugin-node-resolve';
+// doc : https://github.com/rollup/plugins/tree/master/packages/commonjs
 import commonjs from '@rollup/plugin-commonjs';
+// doc : https://github.com/pearofducks/rollup-plugin-dev
+import dev from 'rollup-plugin-dev';
 
 export default {
   input: 'src/index.js',
@@ -25,16 +26,11 @@ export default {
       plugins: [terser()]
     }
   ],
-  sourcemap: 'inline',
   watch: { include: ['src/**', 'public/**'] },
   plugins: [
     resolve({ browser: true }),
     commonjs(),
-    serve({
-      contentBase: ['dist', 'public'],
-      open: true,
-      port: 3000
-    }),
+    dev({ dirs: ['dist', 'public'], port: 3000 }),
     livereload({
       watch: ['dist', 'public']
     }),
