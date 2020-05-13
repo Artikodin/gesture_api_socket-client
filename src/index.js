3f6c0b2c4cc4a5da466534e00afc7ff01ba2b084
mel <melanie.ngo@edu.gobelins.fr>
2020-05-13 19:00:26 +0200

    Handle guid given as queryParam

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import { RelativeOrientationSensor } from 'motion-sensors-polyfill';
 import { initSensor } from './helpers';
 import './helpers/noSleep';
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+if (urlParams.get('guid') != null) {
+  localStorage.setItem('guid', urlParams.get('guid'));
+  history.replaceState({}, document.title, '/');
+}
+
+let guid = localStorage.getItem('guid');
+
 const orientationSensor = new RelativeOrientationSensor({
   frequency: 60,
   referenceFrame: 'device'
@@ -24,6 +33,7 @@ function initialize() {
   initSensor(orientationSensor);
 
   socket.onopen = () => {
+    socket.send({ guid });
     orientationSensor.onreading = () => {
       const [x, y, z, w] = orientationSensor.quaternion;
       console.log({ x, y, z, w });
