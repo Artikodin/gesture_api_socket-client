1294dabe5d28b32a92d81ffd6bdff17dfdd617fb
Artium <artium.wasse@gmail.com>
2020-05-14 01:09:28 +0200

    feat(index): Initialize hammerjs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { RelativeOrientationSensor, LinearAccelerationSensor } from 'motion-sensors-polyfill';
 import { initSensor } from './helpers';
+import Hammer from 'hammerjs';
 import './helpers/noSleep';
 
 const queryString = window.location.search;
@@ -10,6 +11,16 @@ if (urlParams.get('guid') != null) {
 }
 
 let guid = localStorage.getItem('guid');
+
+var touchDetection = document.querySelector('#touchDetection');
+const hammer = new Hammer(touchDetection);
+hammer.get('swipe').set({
+  direction: Hammer.DIRECTION_ALL,
+  threshold: 1,
+  velocity: 0.1
+});
+// Subscribe to a quick start event: press, tap, or doubletap.
+// For a full list of quick start events, read the documentation.
 
 const orientationSensor = new RelativeOrientationSensor({
   frequency: 60,
@@ -38,22 +49,27 @@ const formatTo100th = number => parseFloat(number.toFixed(3));
 function initSocket() {
   noSleepButton.removeEventListener('touchstart', initSocket);
   // initSensor(orientationSensor);
-  initSensor(accelerationSensor);
+  // initSensor(accelerationSensor);
 
   socket.onopen = () => {
     socket.send({ guid });
 
-    accelerationSensor.onreading = () => {
-      const { x, y, z } = accelerationSensor;
-      console.log({ x, y, z });
-      socket.send(
-        JSON.stringify({
-          x: formatTo100th(x),
-          y: formatTo100th(y),
-          z: formatTo100th(z)
-        })
-      );
-    };
+    hammer.on('swipeup swipedown swipeleft swiperight', event => {
+      const { velocity, angle } = event;
+      console.log('swipe', { velocity, angle });
+      socket.send(JSON.stringify({ velocity, angle }));
+    });
+    //   accelerationSensor.onreading = () => {
+    //     const { x, y, z } = accelerationSensor;
+    //     console.log({ x, y, z });
+    //     socket.send(
+    //       JSON.stringify({
+    //         x: formatTo100th(x),
+    //         y: formatTo100th(y),
+    //         z: formatTo100th(z)
+    //       })
+    //     );
+    //   };
     // orientationSensor.onreading = () => {
     //   const [x, y, z, w] = orientationSensor.quaternion;
     //   console.log({
