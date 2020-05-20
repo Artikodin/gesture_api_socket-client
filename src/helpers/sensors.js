f1dcf1d253ecc093f17477d97abb769d9310ac65
Artium <artium.wasse@gmail.com>
2020-05-20 17:54:34 +0200

    feat(helpers): Add sensors helpers

--- src/helpers/sensors.js
+++ src/helpers/sensors.js
@@ -1,3 +1,7 @@
+import { swipeSensor, orientationSensor, accelerationSensor } from '../sensors';
+import { socket } from '../socket';
+import { formatTo100th } from './format';
+
 /**
  *
  * This initialize a sensor on web page. It check if all permissions was allowed.
@@ -40,4 +44,120 @@ export const initSensor = sensor => {
       console.log('Too old iOS device or no permissions to use the sensor.');
     }
   }
+};
+
+export const retrieveSensor = () => {
+  const sensor = localStorage.getItem('sensor');
+  sensor && initialUpdateSensor(sensor);
+};
+
+export const initialUpdateSensor = sensor => {
+  if (localStorage.getItem('sensor') !== sensor) {
+    persistSensor(sensor);
+  }
+  toggleSensorElement(sensor);
+  switchSensor(sensor);
+};
+
+export const updateSensor = sensor => {
+  if (localStorage.getItem('sensor') !== sensor) {
+    persistSensor(sensor);
+    toggleSensorElement(sensor);
+    switchSensor(sensor);
+  }
+};
+
+const persistSensor = sensor => {
+  localStorage.setItem('sensor', sensor);
+};
+
+const toggleSensorElement = sensor => {
+  resetElement();
+  const sensorElement = document.querySelector(`#${sensor}`);
+  sensorElement.classList.add('activatedSensor');
+};
+
+const switchSensor = sensor => {
+  const sensorActions = {
+    gyroscope: () => {
+      resetSensors();
+      initSensor(orientationSensor);
+      orientationEvent();
+    },
+    accelerometer: () => {
+      resetSensors();
+      initSensor(accelerationSensor);
+      accelerationEvent();
+    },
+    swipe: () => {
+      resetSensors();
+      swipeEvent();
+    }
+  };
+  sensorActions[sensor]() || null;
+};
+
+const resetElement = () => {
+  const gyroscopeElement = document.querySelector(`#gyroscope`);
+  const accelerometerElement = document.querySelector(`#accelerometer`);
+  const swipeElement = document.querySelector(`#swipe`);
+
+  gyroscopeElement.classList.contains('activatedSensor') &&
+    gyroscopeElement.classList.remove('activatedSensor');
+  accelerometerElement.classList.contains('activatedSensor') &&
+    accelerometerElement.classList.remove('activatedSensor');
+  swipeElement.classList.contains('activatedSensor') &&
+    swipeElement.classList.remove('activatedSensor');
+};
+
+const swipeEvent = () => {
+  swipeSensor.on('swipeup swipedown swipeleft swiperight', event => {
+    const { velocity, angle } = event;
+    console.log({ velocity, angle });
+    socket.send(JSON.stringify({ velocity, angle }));
+  });
+};
+
+const accelerationEvent = () => {
+  accelerationSensor.onreading = () => {
+    const { x, y, z } = accelerationSensor;
+    console.log({
+      x: formatTo100th(x),
+      y: formatTo100th(y),
+      z: formatTo100th(z)
+    });
+    socket.send(
+      JSON.stringify({
+        x: formatTo100th(x),
+        y: formatTo100th(y),
+        z: formatTo100th(z)
+      })
+    );
+  };
+};
+
+const orientationEvent = () => {
+  orientationSensor.onreading = () => {
+    const [x, y, z, w] = orientationSensor.quaternion;
+    console.log({
+      x: formatTo100th(x),
+      y: formatTo100th(y),
+      z: formatTo100th(z),
+      w: formatTo100th(w)
+    });
+    socket.send(
+      JSON.stringify({
+        x: formatTo100th(x),
+        y: formatTo100th(y),
+        z: formatTo100th(z),
+        w: formatTo100th(w)
+      })
+    );
+  };
+};
+
+const resetSensors = () => {
+  swipeSensor.off('swipeup swipedown swipeleft swiperight');
+  accelerationSensor.activated && accelerationSensor.stop();
+  orientationSensor.activated && orientationSensor.stop();
 };
